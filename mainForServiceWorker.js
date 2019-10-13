f2c30e6f76e121e19d20161ea6ed4b04c85a3471
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-10-13 23:04:22 +0100

    Add service worker javascript module to site

---
+++ mainForServiceWorker.js
@@ -0,0 +1,22 @@
+// Register the service worker in the page's JavaScript
+// The browser will then install it in the backgroud
+
+const registerServiceWorker = () => {
+	if ("serviceWorker" in navigator) {
+		window.addEventListener("load", () => {
+			navigator.serviceWorker.register("/serviceWorker.js").then(
+				registration => {
+					console.log(
+						"Service worker registered successfully in scope of: ",
+						registration.scope
+					);
+				},
+				error => {
+					console.log("Service worker reistration failed: ", error);
+				}
+			);
+		});
+	}
+};
+
+export const serviceWorker = registerServiceWorker();
